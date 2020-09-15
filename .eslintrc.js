6df93ab92751cae361b1d65c652c4d79f5f3d6fe
Marcin Kaminski <marcin.kaminski@schibsted.com>
2020-09-15 19:22:40 +0200

    ✨ Added more eslint checks

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,27 @@ module.exports = {
     'airbnb-base',
     'plugin:jest/style',
     'plugin:jest/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:security/recommended',
+    'plugin:markdown/recommended',
+    'plugin:switch-case/recommended',
+    'plugin:optimize-regex/recommended',
+    'plugin:eslint-comments/recommended',
   ],
-  plugins: ['jest']
+    plugins: [
+    'jest',
+    'sonarjs',
+    'unicorn',
+    'security',
+    'markdown',
+    'filenames',
+    'no-secrets',
+    'switch-case',
+    'optimize-regex',
+    'eslint-comments',
+    'simple-import-sort',
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -18,7 +37,21 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
+  overrides: [
+    {
+      files: ['**/*.md'],
+      processor: 'markdown/markdown',
+    },
+  ],
   rules: {
     "no-console": ["error", { allow: ["warn", "error"] }],
+    'simple-import-sort/sort': 'error',
+    'import/prefer-default-export': 'off',
+    'no-secrets/no-secrets': [
+      'error',
+      { additionalRegexes: { 'Basic Auth': 'Authorization: Basic [A-Za-z0-9+/=]*' } },
+    ],
+    'filenames/match-exported': [2, 'kebab'],
+    'filenames/no-index': 2,
   },
 };
